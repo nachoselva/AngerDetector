84d73c35d085041ccd3f3ee32b9fd55f1bd905f0
nachoselva <nacho.selva1993@gmail.com>
2023-10-07 14:36:52 -0300

    Fix project config (#8)

---
+++ AngerDetector.Tests/ConsoleLogger/TestLogger.cs
@@ -0,0 +1,23 @@
+﻿namespace AngerDetector.Tests.Log
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    internal class TestLogger
+    {
+        public static void LogAutomationTest(string className, string methodName, params object[] parameters)
+        {
+            StringBuilder message = new StringBuilder();
+            message.Append($"Class: {className}");
+            message.Append($" /// ");
+            message.Append($"Method: {methodName}");
+            if (parameters.Any())
+            {
+                message.Append($" /// ");
+                message.Append($"Params: {string.Join(",", parameters)}");
+            }
+            Console.WriteLine(message.ToString());
+        }
+    }
+}
